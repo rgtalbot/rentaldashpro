a81c5100d7e6bef7bdba9d5907333b24b0a31af5
B4Bentley <B4Bentley>
2016-08-15 21:22:31 -0400

    Added google api autocomplete. changed css file to increase z index

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,24 +1,4 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 
-
-
-   // make the modal open for adding a new property
-   $('.modal-trigger').leanModal();
-
-   //api section for trulia TEST
-   $.ajax({
-            url: "http://api.trulia.com/webservices.php?library=LocationInfo&function=getCitiesInState&state=CA&apikey=uyyb7rpb3jkmj7nx73m7bxd8"
-            // data: {
-            //     limit: 10,
-            //     api_key: "dc6zaTOxFJmzC",
-            //     q: search
-            // }
-            , method: 'GET'
-        })
-            .done(function (response) {
-               console.log(response);
-
-         
-        });
     
-});
+// });
