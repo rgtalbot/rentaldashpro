9bc6539ab1805c694bf345dc5ad77210759bb5a4
rgtalbot <rgtalbot@users.noreply.github.com>
2016-08-16 20:37:51 -0400

    doing brent's job for him

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,28 @@
-$(document).ready(function() {
+$(document).on("click",".modalAddBtn",function() {
+    console.log('add button clicked, add property');
 
-    $('.modal-trigger').leanModal();
-    
+    var houseCol = $('<div>').addClass("col-xs-6 col-md-4 col-lg-3");
+
+    var houseDiv = $('<div>').addClass("thumbnail");
+
+    var houseImg = $('<img>').addClass("img-responsive")
+        .attr("src", "assets/images/house2.jpg");
+
+    var houseDiv2 = $('<div>')
+        .addClass("caption");
+
+    var housePara = $('<p>')
+        .addClass("text-center")
+        .text("Simple Information about this house");
+
+    housePara.appendTo(houseDiv2);
+    houseImg.appendTo(houseDiv);
+    houseDiv2.appendTo(houseDiv);
+    houseDiv.appendTo(houseCol);
+
+
+    $('.ownerList').prepend(houseCol);
+
+    $('.modal').modal('toggle');
+
 });
