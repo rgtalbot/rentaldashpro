57854d8ebe42a0e1fb2c8abfaec8007121516040
rgtalbot <rgtalbot@users.noreply.github.com>
2016-08-22 19:24:13 -0400

    changes

--- assets/javascript/overview.js
+++ assets/javascript/overview.js
@@ -9,15 +9,40 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-console.log()
-
-
 
 var count = 0,
-    uniqueID;
+    uniqueID,
+    ownerKey = "4XbtNvOf57REXofdwETCfpiOBKI2";
 
 
-$('.modalAddBtn').click(function() {
+function buildCard() {
+    database.ref('ownerProfiles/' + ownerKey + '/properties/').once('value').then(function (snapshot) {
+        console.log(snapshot.val());
+        $.each(snapshot.val(), function (index, value) {
+            var houseCol = $('<div>').addClass("col-xs-6 col-md-4 col-lg-3");
+            var houseDiv = $('<div>').addClass("rdp-photo-card-property");
+            var houseImg = $('<img>').addClass("rdp-photo-card-img")
+                .attr("src", value.image);
+            var houseDiv2 = $('<div>')
+                .addClass("caption");
+
+            var housePara = $('<p>')
+                .addClass("text-center")
+                .text(value.address);
+
+            housePara.appendTo(houseDiv2);
+            houseImg.appendTo(houseDiv);
+            houseDiv2.appendTo(houseDiv);
+            houseDiv.appendTo(houseCol);
+            $('.ownerList').append(houseCol);
+        });
+    });
+}
+
+buildCard();
+
+
+$('.modalAddBtn').click(function () {
 
     var property =
     {
@@ -27,15 +52,17 @@ $('.modalAddBtn').click(function() {
         bathrooms: $('#prop_bath').val().trim(),
         sqfeet: $('#prop_sqft').val().trim(),
         description: $('#prop_description').val().trim(),
-        type: $('input:radio[name=propertyOptions]:checked').val()
+        type: $('input:radio[name=propertyOptions]:checked').val(),
+        image: 'https://firebasestorage.googleapis.com/v0/b/rental-dash-pro.appspot.com/o/starter.png?alt=media&token=ca604cf1-6b2a-4520-8ec4-fae63a6d50c1',
+        status: 'vacant'
     };
     console.log(property);
     validateForm(property);
-    uniqueID = database.ref('properties').push(property).key;
+    uniqueID = database.ref('ownerProfiles/' + ownerKey + '/properties/').push(property).key;
     console.log(uniqueID);
 
     console.log("count is before if : " + count);
-    if(count==6){
+    if (count == 6) {
         buildCard();
         $('#prop_name').val("");
         $('#prop_address').val("");
@@ -43,9 +70,9 @@ $('.modalAddBtn').click(function() {
         $('#prop_bath').val("");
         $('#prop_sqft').val("");
         $('#prop_description').val("");
-        $('#condo').prop('checked' , false);
-        $('#townHouse').prop('checked' , false);
-        $('#house').prop('checked' , false);
+        $('#condo').prop('checked', false);
+        $('#townHouse').prop('checked', false);
+        $('#house').prop('checked', false);
 
     }
     return false;
@@ -53,116 +80,97 @@ $('.modalAddBtn').click(function() {
 });
 
 
+function validateForm(property) {
+    count = 0;
+    countType = 0;
 
-function validateForm(property){
-    count =0;
-    countType =0;
 
-
-    if(property.propertyName==""){
+    if (property.propertyName == "") {
 
         alert("Please add a Property Name.");
-        count=0;
+        count = 0;
         // return false;
-    }else {
+    } else {
         count++;
     }
 
-    if(property.propertyAddress==""){
+    if (property.propertyAddress == "") {
         alert("Please add a Property Address.");
-        count=0;
+        count = 0;
         // return false;
-    }else {
+    } else {
         count++;
     }
 
-    if(property.bedrooms==""){
+    if (property.bedrooms == "") {
         alert("Please add valid number for bedrooms.");
-        count=0;
+        count = 0;
         // return false;
-    }else if(property.bedrooms<=0){
+    } else if (property.bedrooms <= 0) {
         alert("Please provide a value greater than zero.");
-        count=0;
-    }else {
+        count = 0;
+    } else {
         count++;
     }
 
-    if(property.bathroom==""){
+    if (property.bathroom == "") {
         alert("Please add a valid bathroom count.");
-        count=0;
+        count = 0;
         // return false;
-    }else if(property.bathroom<=0){
+    } else if (property.bathroom <= 0) {
         alert("Please provide a value greater than zero.");
-        count=0;
-    }else {
+        count = 0;
+    } else {
         count++;
     }
 
-    if(property.square==""){
+    if (property.square == "") {
         alert("Please add a valid number for square feet");
-        count=0;
+        count = 0;
         // return false;
-    }else if(property.square<=0){
+    } else if (property.square <= 0) {
         alert("Please provide a value greater than zero.");
-        count=0;
-    }else {
+        count = 0;
+    } else {
         count++;
     }
 
     console.log("count is :" + count);
 
-    if(property.type == "Condo" || property.type == "House" || property.type == "Townhouse" ){
+    if (property.type == "Condo" || property.type == "House" || property.type == "Townhouse") {
         count++;
 
         console.log(property.type);
 
-    }else{
+    } else {
         alert("Please select one valid House type.");
-        count=0;
+        count = 0;
     }
 
-    // if(property.condo == "true" || property.townHouse == "true" || property.house == "true"){
-    //     countType++;
-    //
-    //     console.log("countype is: " + countType);
-    //     // return false;
-    // }
-    //
-    // if(countType >= 1){
-    //     count++;
-    // } else{
-    //     alert("Please select one valid House type.");
-    //     count =0;
-    //     countType = 0;
-    // }
-
-
-
-
 }
 
-function buildCard() {
-    var houseCol = $('<div>').addClass("col-xs-6 col-md-4 col-lg-3");
-
-    var houseDiv = $('<div>').addClass("rdp-photo-card-property");
-
-    var houseImg = $('<img>').addClass("rdp-photo-card-img")
-        .attr("src", "assets/images/house2.jpg");
-
-    var houseDiv2 = $('<div>')
-        .addClass("caption");
-
-    var housePara = $('<p>')
-        .addClass("text-center")
-        .text("Information about this property");
-
-    housePara.appendTo(houseDiv2);
-    houseImg.appendTo(houseDiv);
-    houseDiv2.appendTo(houseDiv);
-    houseDiv.appendTo(houseCol);
-
-
-    $('.ownerList').prepend(houseCol);
-
-    $('.modal').modal('toggle');
-}
+// function buildCard() {
+//     var houseCol = $('<div>').addClass("col-xs-6 col-md-4 col-lg-3");
+//
+//     var houseDiv = $('<div>').addClass("rdp-photo-card-property");
+//
+//     var houseImg = $('<img>').addClass("rdp-photo-card-img")
+//         .attr("src", "http://placehold.it/200x200");
+//
+//     var houseDiv2 = $('<div>')
+//         .addClass("caption");
+//
+//     var housePara = $('<p>')
+//         .addClass("text-center")
+//         .text("Information about this property");
+//
+//     housePara.appendTo(houseDiv2);
+//     houseImg.appendTo(houseDiv);
+//     houseDiv2.appendTo(houseDiv);
+//     houseDiv.appendTo(houseCol);
+//
+//
+//     $('.ownerList').prepend(houseCol);
+//
+//     $('.modal').modal('toggle');
+// }
